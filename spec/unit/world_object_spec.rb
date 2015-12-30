7eab1d76f7eb9f87f02a37dcddac021cdf051415
Samuel Brandão <gb.samuel@gmail.com>
2015-12-30 22:21:37 +0100

    Fixing world_object_spec (just indentation / wrong nesting)

--- spec/unit/world_object_spec.rb
+++ spec/unit/world_object_spec.rb
@@ -32,40 +32,39 @@ describe WorldObject do
       an_object = dummy_klass.new(0, 33)
       expect(an_object.char_map).to eq( { [0,0] => "X" })
     end
+  end
 
-    describe 'anchor_x and anchor_y reader methods' do
-      it 'returns coordinates rounded to the closest number' do
-        an_object = dummy_klass.new(0.333, 33.777)
-        expect(an_object.anchor_x).to eq(0)
-        expect(an_object.anchor_y).to eq(34)
-      end
-
-      it 'stores a real number internally' do
-        an_object = dummy_klass.new(0.5, 1.5)
-        x = an_object.instance_variable_get(:@anchor_x)
-        expect(x).to eq(0.5)
-        y = an_object.instance_variable_get(:@anchor_y)
-        expect(y).to eq(1.5)
-      end
+  describe 'anchor_x and anchor_y reader methods' do
+    it 'returns coordinates rounded to the closest number' do
+      an_object = dummy_klass.new(0.333, 33.777)
+      expect(an_object.anchor_x).to eq(0)
+      expect(an_object.anchor_y).to eq(34)
     end
 
-    describe '#move' do
-      it 'moves the object' do
-        an_object = dummy_klass.new(0, 0)
-        an_object.move(2, 4)
-
-        expect(an_object.anchor_x).to eq 2
-        expect(an_object.anchor_y).to eq 4
-      end
-
-      it 'accepts any real number as moving parameter' do
-        an_object = dummy_klass.new(0.5, 0.8)
-        an_object.move(1.5, 3.2)
-
-        expect(an_object.anchor_x).to eq 2
-        expect(an_object.anchor_y).to eq 4
-      end
+    it 'stores a real number internally' do
+      an_object = dummy_klass.new(0.5, 1.5)
+      x = an_object.instance_variable_get(:@anchor_x)
+      expect(x).to eq(0.5)
+      y = an_object.instance_variable_get(:@anchor_y)
+      expect(y).to eq(1.5)
     end
   end
 
+  describe '#move' do
+    it 'moves the object' do
+      an_object = dummy_klass.new(0, 0)
+      an_object.move(2, 4)
+
+      expect(an_object.anchor_x).to eq 2
+      expect(an_object.anchor_y).to eq 4
+    end
+
+    it 'accepts any real number as moving parameter' do
+      an_object = dummy_klass.new(0.5, 0.8)
+      an_object.move(1.5, 3.2)
+
+      expect(an_object.anchor_x).to eq 2
+      expect(an_object.anchor_y).to eq 4
+    end
+  end
 end
