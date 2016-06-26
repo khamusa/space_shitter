2585b0b489bf258dce1a814f4d7f8210ba0089ad
Samuel Brandão <gb.samuel@gmail.com>
2016-06-26 18:20:27 -0300

    Display world objects count to help debugging garbage collection

--- lib/game_io/console_game_displayer.rb
+++ lib/game_io/console_game_displayer.rb
@@ -67,6 +67,8 @@ class ConsoleGameDisplayer
     @menu_win.addstr("The Spaceshitter Game")
     @menu_win.setpos(2, 1)
     @menu_win.addstr "World size is #{world_width}x#{world_height}"
+    @menu_win.setpos(3, 1)
+    @menu_win.addstr "Objects in world: #{world.objects.count}   "
   end
 
   def draw_objects(drawable_objects)
